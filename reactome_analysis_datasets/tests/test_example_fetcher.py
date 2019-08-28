d9d363191dd2a9df117e90de8bc71939c3237fac
Johannes Griss <jgriss8@yahoo.com>
2019-08-28 22:53:13 +0200

    [datasets] Adapted test case

--- reactome_analysis_datasets/tests/test_example_fetcher.py
+++ reactome_analysis_datasets/tests/test_example_fetcher.py
@@ -16,9 +16,6 @@ class DatasetFetcherTest(unittest.TestCase):
         os.environ["RABBIT_USER"] = "test"
         os.environ["RABBIT_PASSWORD"] = "test"
 
-        # set the data directory
-        os.environ["EXAMPLE_DIRECTORY"] = os.path.join(os.path.dirname(__file__), "testfiles")
-
         logging.basicConfig(level=logging.DEBUG)
         pika_logger = logging.getLogger("pika")
         pika_logger.setLevel(logging.ERROR)
@@ -26,7 +23,14 @@ class DatasetFetcherTest(unittest.TestCase):
         util_logger = logging.getLogger("reactome_analysis_datasets")
         util_logger.setLevel(logging.DEBUG)
 
+    def process_all_messages(self):
+        fetcher = reactome_analysis_dataset_fetcher.ReactomeAnalysisDatasetFetcher()
+        fetcher.start_listening()
+
     def test_fetch_dataset(self):
+        # set the data directory
+        os.environ["EXAMPLE_DIRECTORY"] = os.path.join(os.path.dirname(__file__), "testfiles")
+
         fetcher = reactome_analysis_dataset_fetcher.ReactomeAnalysisDatasetFetcher()
 
         # post one analysis request
@@ -51,6 +55,34 @@ class DatasetFetcherTest(unittest.TestCase):
 
         # get the data
         data_string = storage.get_request_data("EXAMPLE_1")
+        self.assertIsNotNone(data_string)
+
+    def test_fetch_proteomics(self):
+        os.environ["EXAMPLE_DIRECTORY"] = os.path.join(os.path.dirname(__file__), "../example_datasets")
+        fetcher = reactome_analysis_dataset_fetcher.ReactomeAnalysisDatasetFetcher()
+
+        # post one analysis request
+        mq = reactome_mq.ReactomeMQ(queue_name=reactome_mq.DATASET_QUEUE)
+        storage = reactome_storage.ReactomeStorage()
+
+        mq.post_analysis(models.dataset_request.DatasetRequest("loading_2", "EXAMPLE_MEL_PROT").to_json(), method="test")
+
+        # process the message
+        fetcher.process_single_message()
+
+        # make sure the status is complete
+        status = storage.get_status(analysis_identifier="loading_2", data_type="dataset")
+
+        self.assertIsNotNone(status)
+        status_obj = json.loads(status)
+        self.assertEqual("complete", status_obj["status"])
+
+        # get the summary
+        summary_string = storage.get_request_data_summary("EXAMPLE_MEL_PROT")
+        self.assertIsNotNone(summary_string)
+
+        # get the data
+        data_string = storage.get_request_data("EXAMPLE_MEL_PROT")
         self.assertIsNotNone(data_string)
 
 if __name__ == '__main__':
