293b43663dc661c509ed2254183ab0837aa477eb
Johannes Griss <jgriss8@yahoo.com>
2020-01-20 14:15:28 +0100

    [util] Added fields to the report_request object

--- reactome_analysis_utils/reactome_analysis_utils/models/report_request.py
+++ reactome_analysis_utils/reactome_analysis_utils/models/report_request.py
@@ -7,14 +7,19 @@ import json
 
 
 class ReportRequest:
-    def __init__(self, analysis_id: str, user_mail: str = None):
+    def __init__(self, analysis_id: str, user_mail: str = None,
+                 include_interactors: bool = None, include_disease: bool = None):
         """
         Creates a new ReportRequest instance
         :param analysis_id: Analysis id
         :param user_mail: The user's e-mail address (if available)
+        :param include_interactors: Indicates whether interactors were included in the analysis.
+        :param include_disease: Indicates whether disease pathways were included in the analysis.
         """
         self.analysis_id = analysis_id
         self.user_mail = user_mail
+        self.include_interactors = include_interactors
+        self.include_disease = include_disease
 
     def to_json(self) -> str:
         """
@@ -25,6 +30,10 @@ class ReportRequest:
 
         if self.user_mail:
             dict_obj["user_mail"] = self.user_mail
+        if self.include_interactors:
+            dict_obj["include_interactors"] = self.include_interactors
+        if self.include_disease:
+            dict_obj["include_disease"] = self.include_disease
 
         json_str = json.dumps(dict_obj)
 
@@ -47,6 +56,8 @@ def from_json(json_string: str) -> ReportRequest:
             raise Exception("JSON string does not represent a ReportRequest object. Missing " + field)
 
     # create the object
-    request_obj = ReportRequest(analysis_id=dict_obj["analysis_id"], user_mail=dict_obj.get("user_mail", None))
+    request_obj = ReportRequest(analysis_id=dict_obj["analysis_id"], user_mail=dict_obj.get("user_mail", None),
+                                include_interactors=dict_obj.get("include_interactors"),
+                                include_disease=dict_obj.get("include_disease", None))
 
     return request_obj
