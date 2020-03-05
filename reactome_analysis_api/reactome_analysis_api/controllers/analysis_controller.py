c2e788f454a6c122f2aa49ec9fe20d9879d67a05
Johannes Griss <jgriss8@yahoo.com>
2020-03-05 15:47:06 +0100

    [api] Optimise memory consumption

--- reactome_analysis_api/reactome_analysis_api/controllers/analysis_controller.py
+++ reactome_analysis_api/reactome_analysis_api/controllers/analysis_controller.py
@@ -81,6 +81,10 @@ def start_analysis(body):  # noqa: E501
         LOGGER.debug("Received gzipped analysis request. Decompressing...")
 
         decompressed_string = zlib.decompress(connexion.request.data)
+
+        # free the binary data
+        connexion.request.data = None
+
         analysis_dict = json.loads(decompressed_string)
         
         # free the memory again
@@ -89,6 +93,9 @@ def start_analysis(body):  # noqa: E501
         LOGGER.debug("Invalid analysis request submitted. Request body does not describe a JSON object.")
         abort(406, "Invalid analysis request submitted. Request body does not describe a JSON object.")
         return
+
+    # remove the connexion request object
+    connexion.request = None
     
     try:
         analysis_request = input_deserializer.create_analysis_input_object(analysis_dict)
