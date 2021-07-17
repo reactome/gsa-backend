db9832e0b3e19309f97acdc5254b416bf7dcfc0f
Johannes Griss <jgriss8@yahoo.com>
2021-07-17 22:28:25 +0200

    [api/datasets/report/worker] Set log for redisc

--- reactome_analysis_worker/reactome_analysis_worker/__main__.py
+++ reactome_analysis_worker/reactome_analysis_worker/__main__.py
@@ -26,6 +26,7 @@ def main():
     # set config for other packages
     pika_logger = logging.getLogger("pika")
     pika_logger.setLevel(level=logging.ERROR)
+    logging.getLogger("rediscluster").setLevel(logging.ERROR)
 
     worker = ReactomeAnalysisWorker()
 
