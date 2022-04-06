677169b9aac167f34d4eb6297141f37f227f9f89
Johannes Griss <jgriss8@yahoo.com>
2022-04-06 20:08:11 +0200

    [worker] Fixes #18 Experimental design test

--- reactome_analysis_worker/reactome_analysis_worker/testing_tools/experimental_design_tests.py
+++ reactome_analysis_worker/reactome_analysis_worker/testing_tools/experimental_design_tests.py
@@ -18,6 +18,7 @@ class ExperimentalDesignExceptionType(enum.Enum):
     EMPTY_GROUP = 3
     FEW_SAMPLES = 4
     FEW_SAMPLES_COVAR = 5
+    INVALID_DESIGN = 6
 
 
 class ExperimentalDesignException(Exception):
@@ -48,51 +49,64 @@ class ExperimentalDesignTester:
         :param datasets: The datasets to test
         """
         # run the test for every dataset
-        for dataset in datasets:
-            # ignore requests that do not contain a design
-            if not dataset.design:
-                continue
+        try:
+            for dataset in datasets:
+                # ignore requests that do not contain a design
+                if not dataset.design:
+                    continue
 
-            # test for equal samples
-            if not ExperimentalDesignTester._has_equal_samples(dataset.design, dataset.df):
-                raise ExperimentalDesignException(
-                        msg="Different number of samples in the experimental design",
-                        type=ExperimentalDesignExceptionType.FORMAT_ERROR,
-                        dataset_name=dataset.name)
+                # test for equal samples
+                if not ExperimentalDesignTester._has_equal_samples(dataset.design, dataset.df):
+                    raise ExperimentalDesignException(
+                            msg="Different number of samples in the experimental design",
+                            type=ExperimentalDesignExceptionType.FORMAT_ERROR,
+                            dataset_name=dataset.name)
 
-            # test for different comparison groups
-            if not ExperimentalDesignTester._has_different_comparison_groups(dataset.design):
-                raise ExperimentalDesignException(
-                        msg="Comparison group 1 and 2 must be different.",
-                        type=ExperimentalDesignExceptionType.SAME_GROUP,
-                        dataset_name=dataset.name)
-                    
-            # make sure the sample groups are present
-            missing_group = ExperimentalDesignTester._find_missing_comparison_group(dataset.design)
+                # test for different comparison groups
+                if not ExperimentalDesignTester._has_different_comparison_groups(dataset.design):
+                    raise ExperimentalDesignException(
+                            msg="Comparison group 1 and 2 must be different.",
+                            type=ExperimentalDesignExceptionType.SAME_GROUP,
+                            dataset_name=dataset.name)
+                        
+                # make sure the sample groups are present
+                missing_group = ExperimentalDesignTester._find_missing_comparison_group(dataset.design)
 
-            if missing_group:
-                raise ExperimentalDesignException(
-                        msg="No samples annotated as comparison group '{group}'".format(group=missing_group),
-                        type=ExperimentalDesignExceptionType.EMPTY_GROUP,
-                        dataset_name=dataset.name)
+                if missing_group:
+                    raise ExperimentalDesignException(
+                            msg="No samples annotated as comparison group '{group}'".format(group=missing_group),
+                            type=ExperimentalDesignExceptionType.EMPTY_GROUP,
+                            dataset_name=dataset.name)
 
-            # make sure a suffient number of samples are annotated for every group
-            too_few_samples_group = ExperimentalDesignTester._test_sufficient_samples_per_group(dataset.design)
+                # make sure a suffient number of samples are annotated for every group
+                too_few_samples_group = ExperimentalDesignTester._test_sufficient_samples_per_group(dataset.design)
 
-            if too_few_samples_group:
-                raise ExperimentalDesignException(
-                        msg="Insufficient samples in group '{group}'. Each group must at least contain 3 samples for accurate results.".format(group=too_few_samples_group),
-                        type=ExperimentalDesignExceptionType.FEW_SAMPLES,
-                        dataset_name=dataset.name)
+                if too_few_samples_group:
+                    raise ExperimentalDesignException(
+                            msg="Insufficient samples in group '{group}'. Each group must at least contain 3 samples for accurate results.".format(group=too_few_samples_group),
+                            type=ExperimentalDesignExceptionType.FEW_SAMPLES,
+                            dataset_name=dataset.name)
 
-            # make sure that additional groups have sufficient samples
-            insufficient_samples_in_covariate = ExperimentalDesignTester._test_sufficient_samples_per_covariate(dataset.design)
+                # make sure that additional groups have sufficient samples
+                insufficient_samples_in_covariate = ExperimentalDesignTester._test_sufficient_samples_per_covariate(dataset.design)
 
-            if insufficient_samples_in_covariate:
-                raise ExperimentalDesignException(
-                        msg="Insufficient samples in covariate '{covariate}'. Each covariate must at least have two samples per group.".format(covariate=insufficient_samples_in_covariate),
-                        type=ExperimentalDesignExceptionType.FEW_SAMPLES_COVAR,
-                        dataset_name=dataset.name)
+                if insufficient_samples_in_covariate:
+                    raise ExperimentalDesignException(
+                            msg="Insufficient samples in covariate '{covariate}'. Each covariate must at least have two samples per group.".format(covariate=insufficient_samples_in_covariate),
+                            type=ExperimentalDesignExceptionType.FEW_SAMPLES_COVAR,
+                            dataset_name=dataset.name)
+        except ExperimentalDesignException as err:
+            # simply re-raise any already handled issue
+            raise err
+        except Exception as err:
+            # this is unexpected, but also most likely caused by an invlid design
+            logger.error("Failed to check experimental design: %s", str(err))
+            logger.exception(err)
+
+            raise ExperimentalDesignException(
+                msg="Invalid experimental design", 
+                type=ExperimentalDesignExceptionType.INVALID_DESIGN, 
+                dataset_name="") from err
 
     @staticmethod
     def _has_equal_samples(design: Design, data: numpy.ndarray) -> bool:
@@ -194,6 +208,12 @@ class ExperimentalDesignTester:
             instance_counts = dict()
 
             for sample_label in design.additional_properties[property_name]:
+                if not sample_label:
+                    raise ExperimentalDesignException(
+                        f"Missing sample label in {property_name}", 
+                        ExperimentalDesignExceptionType.INVALID_DESIGN, 
+                        "")
+
                 # ignore empty lables
                 sample_label = sample_label.strip()
 
