87a363124710eaf066db29324d507d3f7aa7cc95
ag <alexander.michael.grentner@gmail.com>
2023-06-18 09:31:22 +0200

    Add search feature, indey setup

--- reactome_analysis_datasets/reactome_analysis_datasets/dataset_fetchers/public_data_searcher.py
+++ reactome_analysis_datasets/reactome_analysis_datasets/dataset_fetchers/public_data_searcher.py
@@ -1,36 +1,58 @@
-import ix as ix
-from whoosh.fields import Schema, TEXT, KEYWORD
+from whoosh.fields import Schema, TEXT, KEYWORD, NUMERIC
+from grein_fetcher import GreinFetcher
 from whoosh.index import create_in
 import os
-from grein_fetcher import GreinFetcher
-from expression_atlas_fetcher import ExpressionAtlasFetcher
+from whoosh import index
+from whoosh import qparser
 
+
+schema = Schema(data_source=KEYWORD, id=TEXT(stored=True), title=TEXT(stored=True), species=KEYWORD,
+                description=TEXT(stored=True))
 
 
 class Generate_search_values():
 
     @staticmethod
     def setup_search_events():
-        schema = Schema(load_dataset_information=KEYWORD, title=TEXT, species=KEYWORD, description=TEXT)
 
         if not os.path.exists("index"):
             os.mkdir("index")
+
         ix = create_in("index", schema)
 
         writer = ix.writer()
         fetcher_grein = GreinFetcher()
-        grein_datasets = fetcher_grein.get_available_datasets()
+        grein_datasets = fetcher_grein.get_available_datasets(10)  # fetches
 
         for dataset in grein_datasets:
-            writer.add_document(load_dataset_information="GREIN", title=['title'], species=dataset['species'],
-                                description=['study_summary'])
-
-        """
-        fetcher_expression_atlas = ExpressionAtlasFetcher()  # not merged yet
-        fetcher_expression_atlas.get_available_datasets()
-        
-        for dataset in grein_datasets:
-            writer.add_document(load_dataset_information="Expression Atlas", title=['title'], species=dataset['species'],
-                                description=['study_summary'])
-        """
+            writer.add_document(data_source="GREIN", id=str(dataset['geo_accession']), title=str(dataset['title']), species=str(dataset['species']),
+                                description=str(dataset['study_summary']))
         writer.commit()
+
+
+class Searcher():
+
+    dirname =""
+    def __init__(self):
+        self.dirname= "index"
+
+    def index_search(self, search_fields, keyword):
+        ix = index.open_dir(self.dirname)
+        schema = ix.schema
+
+        og = qparser.OrGroup.factory(0.9)
+        mp = qparser.MultifieldParser(search_fields, schema, group=og)
+
+        keyword_parser = mp.parse(keyword)
+
+        with ix.searcher() as s:
+            results = s.search(keyword_parser, terms=True, limit=100)
+            result_dict = {}
+            for result in results:
+                result_dict[result['id']] = result.fields()
+            return result_dict
+
+
+Generate_search_values.setup_search_events()
+search = Searcher()
+result_dict = search.index_search (['description', 'title'], "TFIIH kinase")
