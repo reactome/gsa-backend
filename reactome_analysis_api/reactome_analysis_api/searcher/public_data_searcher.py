d8d5f8dc4be62545a4da2713d5e6eb89146d22ac
ag <alexander.michael.grentner@gmail.com>
2023-11-03 17:28:30 +0100

    add whitlist based build

--- reactome_analysis_api/reactome_analysis_api/searcher/public_data_searcher.py
+++ reactome_analysis_api/reactome_analysis_api/searcher/public_data_searcher.py
@@ -23,18 +23,20 @@ class PublicDatasetSearcher():
     _path = ""
     _species_list = None
     _ix = None
+    _path_whitelist = ""
 
     schema = Schema(data_source=TEXT(stored=True), id=TEXT(stored=True), title=TEXT(stored=True), species=TEXT(stored=True),
                     description=TEXT(stored=True), no_samples=NUMERIC(stored=True), technology=TEXT(stored=True),
                     resource_id=TEXT(stored=True), loading_parameters=TEXT(stored=True))
 
-    def __init__(self, path: str):
+    def __init__(self, path: str, path_whitelist: str):
         """Initialize the public data searcher
 
         :param path: Path to where the index is stored
         :type path: str
         """
         self._path = path
+        self._path_whitelist = path_whitelist
 
     def setup_search_events(self):  
         """
@@ -51,27 +53,41 @@ class PublicDatasetSearcher():
         writer = ix.writer()
 
         LOGGER.debug("Fetching available datasets")
-        
+
+        whitelist = self._get_whitelist_datasets(self._path_whitelist)
         datasets = PublicDataFetcher.get_available_datasets()
 
         for dataset in datasets:
             # ignore datasets without an id (happens sometimes in GREIN)
             if not "id" in dataset or type(dataset["id"]) != str or len(dataset["id"].strip()) < 3:
                 continue
-
-            writer.add_document(data_source=str(dataset['resource_id_str']), id=str(dataset['id']), title=str(dataset['title']),
-                                species=str(dataset['species']),
-                                description=str(dataset['study_summary']), no_samples=str(dataset['no_samples']),
-                                technology=str(dataset['technology']),
-                                resource_id=str(dataset['resource_id']),
-                                loading_parameters=str(dataset['loading_parameters']))
-
+            
+            if dataset['id'] in whitelist:
+                writer.add_document(data_source=str(dataset['resource_id_str']), id=str(dataset['id']), title=str(dataset['title']),
+                                    species=str(dataset['species']),
+                                    description=str(dataset['study_summary']), no_samples=str(dataset['no_samples']),
+                                    technology=str(dataset['technology']),
+                                    resource_id=str(dataset['resource_id']),
+                                    loading_parameters=str(dataset['loading_parameters']))
         writer.commit()
 
         # gets species based on public datasets
         species_in_datasets = self._get_species(datasets=datasets) 
         with open(self._path + 'species.pickle', 'wb') as f:
             pickle.dump(species_in_datasets, f, pickle.HIGHEST_PROTOCOL)
+
+    def _get_whitelist_datasets(self, path) -> list:
+        """ 
+        based on the whitelist datasets are fetched from the public datasets
+        :param: path to whitelist 
+        :return: list of datasets in whitelist used to build the index
+        """
+        whitelist = []
+        list_reader = open(path, "r")
+        for line in list_reader.readlines():
+            whitelist.append(line.strip())
+        list_reader.close()
+        return whitelist
 
     def _get_species(self, datasets) -> set:
         """
@@ -153,7 +169,7 @@ class PublicDatasetSearcher():
         
 @click.command()
 @click.option('--path', default=None, help="If set, the path to store the search index in. Otherwise the environment variable 'SEARCH_INDEX_PATH' is used.")
-def create_search_index(path):
+def create_search_index(path, path_whitelist):
     """Create the initial search index.
     """
     # set the logging
@@ -163,6 +179,9 @@ def create_search_index(path):
     if not path:
         path = os.getenv("SEARCH_INDEX_PATH", "../")
 
+    if not path_whitelist:
+        path_whitelist = os.getenv("SEARCH_INDEX_WHITELIST", "../")
+
     # create the search
-    searcher = PublicDatasetSearcher(path=path)
+    searcher = PublicDatasetSearcher(path=path, path_whitelist=path_whitelist)
     searcher.setup_search_events()
