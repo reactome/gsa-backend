8c5ecdbd44d33c4ee6debf6b3912ed8826fe753d
Johannes Griss <jgriss8@yahoo.com>
2024-03-30 07:04:14 +0100

    [docker] Fixed build_image script

--- docker/build_images.sh
+++ docker/build_images.sh
@@ -68,7 +68,7 @@ ${SUDO_CMD} echo ""
 # update the public dataset list
 if [ "${UPDATE_PUBLIC_DATA}" == "y" ]; then
 	echo "Updating GREIN dataset list..."
-    python3 update_grein.py --path_whitellist ../reactome_analysis_api/reactome_analysis_api/searcher/whitelist.txt
+    python3 update_grein.py --path_whitelist ../reactome_analysis_api/reactome_analysis_api/searcher/whitelist.txt
 fi
 
 # Update the Reactome mappings (if set)
