4f0818319ff3e6db98c31be6a3fe8a6e428ae989
agrent <alexander.michael.grentner@gmail.com>
2024-08-13 13:29:48 +0100

    rm Method and ref External data

--- reactome_analysis_api/reactome_analysis_api/controllers/datasets_controller.py
+++ reactome_analysis_api/reactome_analysis_api/controllers/datasets_controller.py
@@ -43,6 +43,9 @@ def get_examples():  # noqa: E501
         ExternalData(id="EXAMPLE_SC_B_CELLS", title="B cell scRNAseq example", type="rnaseq_counts",
                      description="Single-cell RNA-seq data of B cells extracted from the Jerby-Arnon at al. study (Cell 2018).",
                      group="SC_EXAMPLES"),
+        ExternalData(id="EXAMPLE_RIBO_SEQ", title="Ribo seq data example", type="rnaseq_counts",
+                     description="Ribo seq data requires Ribo seq and RNA seq data.",
+                     group="RIBO_EXAMPLES")
     ]
 
 
