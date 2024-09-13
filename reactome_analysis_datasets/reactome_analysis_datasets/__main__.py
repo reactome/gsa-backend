e34e1722a31d0cb16d99406adaf5c7c61819499b
Johannes Griss <jgriss8@yahoo.com>
2024-09-13 21:45:31 +0200

    [datasets] Reduced logging for urllib3

--- reactome_analysis_datasets/reactome_analysis_datasets/__main__.py
+++ reactome_analysis_datasets/reactome_analysis_datasets/__main__.py
@@ -26,6 +26,7 @@ def main():
     pika_logger = logging.getLogger("pika")
     pika_logger.setLevel(level=logging.ERROR)
     logging.getLogger("redis").setLevel(logging.ERROR)
+    logging.getLogger("urllib3").setLevel(logging.ERROR)
 
     dataset_worker = ReactomeAnalysisDatasetFetcher()
 
