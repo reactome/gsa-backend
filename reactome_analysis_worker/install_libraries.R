3ee7567bd441def2f61ce02618545456ee450f5b
agrent <alexander.michael.grentner@gmail.com>
2024-09-16 16:08:19 +0100

    install libs depend

--- reactome_analysis_worker/install_libraries.R
+++ reactome_analysis_worker/install_libraries.R
@@ -6,7 +6,7 @@
 # set the CRAN mirror to use
 options(repos=structure(c(CRAN = 'http://cran.ma.imperial.ac.uk')))
 
-required_libraries <- c("limma", "edgeR", "GSVA", "PADOG")
+required_libraries <- c("limma", "edgeR", "GSVA", "PADOG", "DESeq2", "apeglm")
 
 if (!requireNamespace("BiocManager", quietly = TRUE))
 	install.packages("BiocManager")
@@ -14,7 +14,10 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
 if (!requireNamespace("plyr", quietly = TRUE))
 	install.packages("plyr")
 
-for (library_name in required_libraries) {
+if (!requireNamespace("dplyr", quietly = TRUE))
+	install.packages("dplyr")
+
+for (library_name in required_libraries) {a
 	BiocManager::install(library_name)
 }
 
