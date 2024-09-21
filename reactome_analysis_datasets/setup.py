8f18e3ec25ec5ea3d04311a435232f7cb9ff40f6
Johannes Griss <jgriss8@yahoo.com>
2024-09-21 22:28:05 +0200

    [datasets] Rewrote geo_fetcher to only use the GEOquery R library
    GEOparse showed performance issues as each SOFT file was downloaded individually. Additionally, parsing of the metadata was extremely complex

--- reactome_analysis_datasets/setup.py
+++ reactome_analysis_datasets/setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-requirements = ['reactome_analysis_utils', 'grein_loader', 'GEOparse']
+requirements = ['reactome_analysis_utils', 'grein_loader']
 
 setup_requirements = [ ]
 
