27ebe634e758372d17f6abd817b66d499e331aa8
Johannes Griss <jgriss8@yahoo.com>
2024-09-21 11:22:31 +0200

    [datasets] Adapted status progress for geo_microarray

--- reactome_analysis_datasets/reactome_analysis_datasets/dataset_fetchers/geo_fetcher.py
+++ reactome_analysis_datasets/reactome_analysis_datasets/dataset_fetchers/geo_fetcher.py
@@ -48,7 +48,7 @@ class GeoFetcher(DatasetFetcher):
         # load the data
         LOGGER.info(f"Loading dataset {identifier} from GEO")
         try:
-            self._update_status(progress=0.1, message="Fetching metadata files...")
+            self._update_status(progress=0.2, message="Fetching metadata files...")
             gse = geoparser.get_GEO(identifier)  # fetching data from Geo via geo parser
         except Exception as e:
             LOGGER.error(f"Error loading dataset {identifier} from GEO: {e}")
@@ -60,7 +60,7 @@ class GeoFetcher(DatasetFetcher):
             raise DatasetFetcherException(f"Error loading dataset {identifier} from GEO: No metadata found")
         else:
             LOGGER.info(f"Creating metadata for dataset {identifier}")
-            self._update_status(progress=0.3, message="Creating metadata object...")
+            self._update_status(progress=0.4, message="Downloading detailed metadata...")
             experiment_type = self._get_data_type(gse.metadata)
             sample_metadata_list = self._create_sample_metadata(gse.metadata["sample_id"])
             metadata_obj = ExternalData(id=identifier,
@@ -72,7 +72,7 @@ class GeoFetcher(DatasetFetcher):
                                         sample_metadata=sample_metadata_list,
                                         default_parameters=None)
 
-        self._update_status(progress=0.6, message="Downloading expression data...")
+        self._update_status(progress=0.7, message="Downloading expression data...")
         count_matrix = self._create_count_matrix(identifier)
         os.remove(identifier + "_family.soft.gz")  # clean up supplementary
         self._clean_up_samples(sample_metadata_list[1].values)  # clean up of downloaded files
