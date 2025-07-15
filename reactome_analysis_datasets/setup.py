079d6c0d3b31a45b000d007b7ccca33d53a5577d
Johannes Griss <jg@griss.co.at>
2025-07-15 10:14:11 +0200

    [datasets] Moving to grein_loader 0.0.6

--- reactome_analysis_datasets/setup.py
+++ reactome_analysis_datasets/setup.py
@@ -5,7 +5,7 @@
 
 from setuptools import setup, find_packages
 
-requirements = ['reactome_analysis_utils', 'grein_loader']
+requirements = ['reactome_analysis_utils', 'grein_loader>=0.0.6']
 
 setup_requirements = [ ]
 
